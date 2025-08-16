5def74d18a7bd175721e6a3028fb19676a31d15a
Anton Kulakov <anton.kulakoff@ya.ru>
2025-08-16 22:53:52 +0300

    refactor: updated package config

--- internal/app/camControl/config/config.go
+++ internal/app/camControl/config/config.go
@@ -52,25 +52,3 @@ func getEnv(key, fallback string) string {
 	}
 	return fallback
 }
-
-func NewLogger(logLevel string) *slog.Logger {
-	var level slog.Level
-	switch logLevel {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		level = slog.LevelInfo
-	}
-
-	handlerOptions := &slog.HandlerOptions{
-		Level:     level,
-		AddSource: logLevel == "debug", // Добавляем исходный код только для debug
-	}
-	return slog.New(slog.NewJSONHandler(os.Stdout, handlerOptions))
-}
